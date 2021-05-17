403e5b9375ace500b902bff541b6084f0f4bd312
Niklas Keller <me@kelunik.com>
2021-05-17 19:45:52 +0200

    Allow usage without Fiber being available

--- src/FiberLocal.php
+++ src/FiberLocal.php
@@ -12,25 +12,31 @@ final class FiberLocal
         return $instance;
     }
 
+    /**
+     * @var FiberLocal|null Allows FiberLocal to be used without fiber support.
+     */
+    private ?self $legacy;
+
     private \WeakMap $storage;
     private \Closure $initial;
 
     public function __construct()
     {
         $this->storage = new \WeakMap;
+        $this->legacy = \class_exists(\Fiber::class, false) ? null : $this;
     }
 
     public function set(mixed $value): void
     {
-        $this->storage[\Fiber::this() ?? $this] = $value;
+        $this->storage[$this->legacy ?? \Fiber::this() ?? $this] = $value;
     }
 
     public function get(): mixed
     {
         if (isset($this->initial)) {
-            return $this->storage[\Fiber::this() ?? $this] ?? ($this->initial)();
+            return $this->storage[$this->legacy ?? \Fiber::this() ?? $this] ?? ($this->initial)();
         }
 
-        return $this->storage[\Fiber::this() ?? $this] ?? null;
+        return $this->storage[$this->legacy ?? \Fiber::this() ?? $this] ?? null;
     }
 }
